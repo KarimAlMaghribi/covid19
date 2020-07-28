3b2b6352b57df1befc371fbfe00000a8ab041945
Rakia, Karim <Karim.Rakia@adesso.de>
2020-07-28 12:55:23 +0200

    Eureka-Registery & Client

---
+++ COVID19-client/src/main/java/com/example/COVID19client/Covid19ClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.COVID19client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19ClientApplication.class, args);
+	}
+
+}
